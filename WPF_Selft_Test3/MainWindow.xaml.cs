bc32f81844cac25160852914ce875385db392f7a
henimex <49072861+henimex@users.noreply.github.com>
2021-02-23 23:16:32 +0300

    Small Changes

--- WPF_Selft_Test3/MainWindow.xaml.cs
+++ WPF_Selft_Test3/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace WPF_Selft_Test3
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double lastNumber, result;
+        private double _lastNumber, _result;
+        private SelectedOperator _selectedOperator;
 
         public MainWindow()
         {
@@ -33,24 +34,44 @@ namespace WPF_Selft_Test3
 
         private void BtnEquals_Click(object sender, RoutedEventArgs e)
         {
+            double newNumber;
+            if (double.TryParse(ResultLabel.Content.ToString(), out newNumber))
+            {
+                switch (_selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        _result = SimpleMath.Add(_lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Subtraction:
+                        _result = SimpleMath.Subs(_lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        _result = SimpleMath.Multiple(_lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        _result = SimpleMath.Divide(_lastNumber, newNumber);
+                        break;
+                }
 
+                ResultLabel.Content = _result.ToString();
+            }
         }
 
         private void BtnPercentage_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            if (double.TryParse(ResultLabel.Content.ToString(), out _lastNumber))
             {
-                lastNumber = lastNumber / 100;
-                ResultLabel.Content = lastNumber.ToString();
+                _lastNumber = _lastNumber / 100;
+                ResultLabel.Content = _lastNumber.ToString();
             }
         }
 
         private void BtnMinPlus_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            if (double.TryParse(ResultLabel.Content.ToString(), out _lastNumber))
             {
-                lastNumber = lastNumber * -1;
-                ResultLabel.Content = lastNumber.ToString();
+                _lastNumber = _lastNumber * -1;
+                ResultLabel.Content = _lastNumber.ToString();
             }
         }
 
@@ -62,7 +83,7 @@ namespace WPF_Selft_Test3
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
             int selectedValue = Convert.ToInt32(((Button)sender).Content.ToString());
-            MessageBox.Show($"Pressed Button Is: {selectedValue}");
+            //MessageBox.Show($"Pressed Button Is: {selectedValue}");
 
             if (ResultLabel.Content.ToString() == "0")
             {
@@ -72,6 +93,57 @@ namespace WPF_Selft_Test3
             {
                 ResultLabel.Content = $"{ResultLabel.Content}{selectedValue}";
             }
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out _lastNumber)) ResultLabel.Content = "0";
+
+            if (sender == BtnMultiple) _selectedOperator = SelectedOperator.Multiplication;
+            if (sender == BtnPlus) _selectedOperator = SelectedOperator.Addition;
+            if (sender == BtnMinus) _selectedOperator = SelectedOperator.Subtraction;
+            if (sender == BtnDivide) _selectedOperator = SelectedOperator.Division;
+        }
+
+        private void BtnComa_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!ResultLabel.Content.ToString().Contains(".")) ResultLabel.Content = $"{ResultLabel.Content}.";
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double Subs(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public static double Multiple(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double Divide(double number1, double number2)
+        {
+            if (number2 == 0)
+            {
+                MessageBox.Show("Divide by 0 is 0");
+                return 0;
+            }
+            return number1 / number2;
         }
     }
 }
