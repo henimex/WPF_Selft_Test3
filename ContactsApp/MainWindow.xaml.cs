b5ffee7d4f5c0fd268d03adebebd777eda6a4539
J.Henimex <J.Henimex@IT-01>
2021-02-24 16:15:21 +0300

    End of the day

---
+++ ContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ContactsApp.Entities;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> _contacts;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _contacts = new List<Contact>();
+            ReadDB();
+        }
+
+        private void BtnNewContact_OnClick(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+            ReadDB();
+        }
+
+        void ReadDB()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                _contacts = connection.Table<Contact>().ToList().OrderBy(x => x.Name).ToList();
+            }
+
+            if (_contacts != null)
+            {
+                //foreach (var contact in contacts)
+                //{
+                //    ListVContacts.Items.Add(new ListViewItem()
+                //    {
+                //        Content = contact.Name
+                //    });
+                //}
+
+                ListVContacts.ItemsSource = _contacts;
+            }
+        }
+
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                TextBox searchTextBox = sender as TextBox;
+                var filteredList = _contacts.Where(x => x.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+                ListVContacts.ItemsSource = filteredList;
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ ContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ContactsApp.Entities;
+using SQLite;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            Contact contact = new Contact()
+            {
+                Name = TxtName.Text,
+                Email = TxtEmail.Text,
+                Phone = TxtPhone.Text
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            Close();
+        }
+    }
+}
